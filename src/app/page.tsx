9f886ca3997fd2d9ba53b6d73ebf96f29c6f879a
masakinihirota <masakinihirota@gmail.com>
2023-06-13 04:38:08 +0900

    📝 ドキュメントの微修正 ⚙ update 5 files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,24 @@ import Link from "next/link"
 import Auth from "@/components/Auth"
 import { useAuth, VIEWS } from "@/components/Auth/AuthProvider"
 
+// src/pages/index.tsx
 export default function Home() {
+  // 1. ユーザーの認証状態を取得する
   const { initial, user, view, signOut } = useAuth()
 
+  // 2. ページの最初のレンダーが完了するまで、
+  //    ユーザーの認証状態を待ちます
   if (initial) {
     return <div>Loading...</div>
   }
 
+  // 3. ユーザーがパスワードを更新する必要がある場合は、
+  //    パスワード更新フォームを表示します
   if (view === VIEWS.UPDATE_PASSWORD) {
     return <Auth view={view} />
   }
 
+  // 4. それ以外の場合は、ユーザーに対して適切なコンテンツを表示します
   return (
     <div>
       {user ? (
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,13 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "media",
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
+  // theme: {
+  //   extend: {
+  //     backgroundImage: {
+  //       "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+  //       "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+  //     },
+  //   },
+  // },
   plugins: [forms],
 }
